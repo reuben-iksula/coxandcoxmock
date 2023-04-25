da671cc8ff2430cca8c0200eb97ca04b60869e6d
ReubenBarboza <reuben.b@iksula.com>
2023-04-25 14:33:16 +0530

    added double-thumb-range slider

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./index.css";
 import Alpine from "alpinejs";
 import Glide from "@glidejs/glide";
+import noUiSlider from "nouislider";
+import "nouislider/dist/nouislider.css";
 
 window.Alpine = Alpine;
 
@@ -23,6 +25,31 @@ Alpine.data("imgslider", () => ({
     const glideElement = document.querySelector(".glide");
     this.glide = new Glide(glideElement, this.glideOptions);
     this.glide.mount();
+  },
+}));
+
+Alpine.data("doubleThumbRange", () => ({
+  vals: [],
+  slider: null,
+  init() {
+    this.slider = document.querySelector("#double-thumb-range");
+    noUiSlider.create(this.slider, {
+      start: [0, 5759],
+      connect: true,
+      range: {
+        min: 0,
+        max: 5759,
+      },
+    });
+    this.slider.noUiSlider.on("update", (values, handle) => {
+      this.setVals(values);
+    });
+  },
+
+  setVals() {
+    // let el = document.querySelector("#double-thumb-range .noUi-handle-lower");
+    // this.leftVal = `£${el.ariaValueNow}`;
+    this.vals = this.slider.noUiSlider.get();
   },
 }));
 
